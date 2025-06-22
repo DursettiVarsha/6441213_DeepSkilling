bc76b379dbe5fddc2cfdfefb4bb22dcb5c639f0a
Dursetti Varsha <dhjvarshav230205@gmail.com>
2025-06-23 00:08:08 +0530

    Delete hands-on-7_FinancialForecasting directory

--- hands-on-7_FinancialForecasting/code/Forecast.java
+++
@@ -1,9 +0,0 @@
-public class Forecast {
-
-    public static double forecastRecursive(double presentValue, double growthRate, int years) {
-        if (years == 0) {
-            return presentValue;
-        }
-        return forecastRecursive(presentValue, growthRate, years - 1) * (1 + growthRate);
-    }
-}
