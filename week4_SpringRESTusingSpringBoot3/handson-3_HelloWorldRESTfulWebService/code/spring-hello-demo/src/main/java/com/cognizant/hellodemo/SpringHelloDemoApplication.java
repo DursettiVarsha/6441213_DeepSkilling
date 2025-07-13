e53ae4c11769cda381745e375600943aa1b8d61b
Dursetti Varsha <dhjvarshav230205@gmail.com>
2025-07-14 00:51:42 +0530

    first commit

---
+++ week4_SpringRESTusingSpringBoot3/handson-3_HelloWorldRESTfulWebService/code/spring-hello-demo/src/main/java/com/cognizant/hellodemo/SpringHelloDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.hellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelloDemoApplication.class, args);
+	}
+
+}
